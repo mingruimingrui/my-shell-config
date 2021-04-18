ce5805de3b27ea52a496825887f05476f425373d
mingruimingrui <mingruimingrui@hotmail.com>
2021-04-18 16:06:30 +0800

    Finish setup script

--- setup.sh
+++ setup.sh
@@ -18,18 +18,25 @@ fi
 
 # Download repository
 REPO_DIR=~/.my-shell-config
-REPO_URL=https://github.com/mingruimingrui/my-shell-config.git
-if [ ! -z "$USE_SSH" ]; then
-	REPO_URL=git@github.com:mingruimingrui/my-shell-config.git
+if [ ! -d "$REPO_DIR" ]; then
+	echo "Downloading my-shell-config"
+	REPO_URL=https://github.com/mingruimingrui/my-shell-config.git
+	if [ ! -z "$USE_SSH" ]; then
+		REPO_URL=git@github.com:mingruimingrui/my-shell-config.git
+	fi
+	git clone $REPO_URL $REPO_DIR
 fi
-git clone $REPO_URL $REPO_DIR
 cd $REPO_DIR
-echo $PWD
 
 # Symlink files over
 mkdir -p ~/.config/fish
-# ln -sf config.fish ~/.config/fish/config.fish
-# ln -sf custom.fish ~/.config/fish/custom.fish
+cp $REPO_DIR/config.fish ~/.config/fish/config.fish  # config.fish is a stub
+ln -sf $REPO_DIR/custom.fish ~/.config/fish/custom.fish
 
 mkdir -p ~/.config/nvim
-# ln -sf init.vim ~/.config/nvim/init.vim
+ln -sf $REPO_DIR/init.vim ~/.config/nvim/init.vim
+
+# Run conda init to modify config.fish
+if [ -d "~/miniconda3/bin" ]; then
+	~/miniconda3/bin/conda init fish
+fi
