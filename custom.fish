2077441a3c76fb9892843dd95b52178947f1b8c9
Wang Ming Rui <mingrui.wang@shopee.com>
2021-04-20 12:10:40 +0800

    Added pyclean

--- custom.fish
+++ custom.fish
@@ -38,11 +38,23 @@ if type -q brew
 	end
 end
 
-# Conda
+# Python
 if type -q conda
 	function conda-upgrade-base -d "Upgrade base default conda"
 		conda update -n base -c defaults conda
 	end
+end
+
+function pyclean -d "Recursively clean directory from .pyc and .pyo files and python3 __pycache__ folders"
+  set -l path2CLEAN
+
+  if set -q $argv
+    set path2CLEAN .
+  else
+    set path2CLEAN $argv
+  end
+
+  find $path2CLEAN -type f -name "*.py[co]" -delete -or -type d -name "__pycache__" -delete
 end
 
 # Kubernetes
