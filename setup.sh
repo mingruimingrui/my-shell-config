b8f9f39db0f3b6dda22b1fc5057e00a12cb0e6d4
Wang Ming Rui <mingrui.wang@shopee.com>
2021-05-18 11:18:22 +0800

    Use whitespace over tab

--- setup.sh
+++ setup.sh
@@ -3,46 +3,45 @@ set -e
 
 # starship
 if ! command -v starship > /dev/null; then
-	echo "Installing starship into ~/.local/bin"
-	curl -fsSL -o install.sh https://starship.rs/install.sh
-	mkdir -p ~/.local/bin
-	sh install.sh -b ~/.local/bin -f
-	rm install.sh
+  echo "Installing starship into ~/.local/bin"
+  curl -fsSL -o install.sh https://starship.rs/install.sh
+  mkdir -p ~/.local/bin
+  sh install.sh -b ~/.local/bin -f
+  rm install.sh
 fi
 
 # Download repository
 REPO_DIR=~/.my-shell-config
 if [ ! -d "$REPO_DIR" ]; then
-	echo "Downloading my-shell-config"
-	REPO_URL=https://github.com/mingruimingrui/my-shell-config.git
-	if [ ! -z "$USE_SSH" ]; then
-		REPO_URL=git@github.com:mingruimingrui/my-shell-config.git
-	fi
-	git clone $REPO_URL $REPO_DIR
+  echo "Downloading my-shell-config"
+  REPO_URL=https://github.com/mingruimingrui/my-shell-config.git
+  if [ ! -z "$USE_SSH" ]; then
+    REPO_URL=git@github.com:mingruimingrui/my-shell-config.git
+  fi
+  git clone $REPO_URL $REPO_DIR
 fi
 cd $REPO_DIR
 
-# Symlink files over
-mkdir -p ~/.config/fish
+# Symlink/copy files over
+mkdir -p ~/.config/fish ~/.config/nvim
 cp $REPO_DIR/config.fish ~/.config/fish/config.fish  # config.fish is a stub
 ln -sf $REPO_DIR/custom.fish ~/.config/fish/custom.fish
-
-mkdir -p ~/.config/nvim
 ln -sf $REPO_DIR/init.vim ~/.config/nvim/init.vim
 
 # Run conda init to modify config.fish
 if [ -d "~/miniconda3/bin" ]; then
-	~/miniconda3/bin/conda init fish
+  echo "Running conda init"
+  ~/miniconda3/bin/conda init fish
 fi
 
 # Install neovim packages
 if command -v nvim > /dev/null; then
-	nvim +PlugClean +PlugUpgrade +PlugUpdate +qall | true
-	nvim +GoUpdateBinaries +qall | true
+  echo "Installing neovim plugins"
+  nvim +PlugClean +PlugUpgrade +PlugUpdate +qall | true
+  nvim +GoUpdateBinaries +qall | true
 fi
 
 # Reminder to install fish
 if ! comamnd -v fish > /dev/null; then
-	echo "Remember to install fish later!"
+  echo "Remember to install fish later!"
 fi
-
